6ebb3f96f83142bb89f5fc14e68cbc5f5ab90e2c
zhouhongyin <1429855087@qq.com>
2022-11-22 09:22:22 +0800

    子集

---
+++ top100/src/main/java/leetcode/editor/cn/P78Subsets.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0]
+//输出：[[],[0]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// nums 中的所有元素 互不相同
+//
+//
+// Related Topics 位运算 数组 回溯 👍 1862 👎 0
+
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+//Java：子集
+public class P78Subsets {
+    public static void main(String[] args) {
+        Solution solution = new P78Subsets().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> path = new ArrayDeque<>();
+
+        int n;
+        int[] nums;
+        public List<List<Integer>> subsets(int[] nums) {
+            this.nums = nums;
+            this.n = nums.length;
+
+            backtracking(0);
+
+            return res;
+        }
+
+        public void backtracking(int startIndex) {
+            res.add(new ArrayList<>(path));
+
+            if (path.size() == n) {
+                return;
+            }
+
+            for (int i = startIndex; i < n; i++) {
+                path.push(nums[i]);
+                backtracking(i + 1);
+                path.pop();
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
