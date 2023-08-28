b84681b715a0e869fc71fc0f74c945819f7d5f19
zhouhongyin <1429855087@qq.com>
2023-08-28 09:16:40 +0800

    64. 最小路径和

---
+++ hot100/src/main/java/leetcode/editor/cn/P64MinimumPathSum.java
@@ -0,0 +1,75 @@
+package leetcode.editor.cn;
+//给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//
+// 说明：每次只能向下或者向右移动一步。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+//输出：7
+//解释：因为路径 1→3→1→1→1 的总和最小。
+//
+//
+// 示例 2：
+//
+//
+//输入：grid = [[1,2,3],[4,5,6]]
+//输出：12
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 200
+// 0 <= grid[i][j] <= 200
+//
+//
+// Related Topics 数组 动态规划 矩阵 👍 1562 👎 0
+
+
+//Java：最小路径和
+public class P64MinimumPathSum {
+    public static void main(String[] args) {
+        Solution solution = new P64MinimumPathSum().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minPathSum(int[][] grid) {
+            int n = grid.length;
+            int m = grid[0].length;
+
+
+
+            for (int i = 1; i < n; i++) {
+                 grid[i][0] += grid[i - 1][0];
+            }
+
+
+            for (int j = 1; j < m; j++) {
+                grid[0][j] += grid[0][j - 1];
+            }
+
+            if (n == 1 || m == 1) return grid[n - 1][m - 1];
+
+
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < m; j++) {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+
+            return grid[n - 1][m - 1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
