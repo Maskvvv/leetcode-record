3e611f7048565f283b0d1b643a246d21300e67e5
mask <1429855087@qq.com>
2023-09-03 15:03:03 +0800

    1143. 最长公共子序列

--- hot100/src/main/java/leetcode/editor/cn/P1143LongestCommonSubsequence.java
+++ hot100/src/main/java/leetcode/editor/cn/P1143LongestCommonSubsequence.java
@@ -59,13 +59,12 @@ public class P1143LongestCommonSubsequence {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestCommonSubsequence(String text1, String text2) {
-            int n = text1.length(), m = text2.length();
+            int m = text1.length(), n = text2.length();
 
-            int[][] dp = new int[n + 1][m + 1];
+            int[][] dp = new int[m + 1][n + 1];
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= m; j++) {
-
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
                     if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
                     } else {
@@ -73,10 +72,9 @@ public class P1143LongestCommonSubsequence {
                     }
 
                 }
-
             }
 
-            return dp[n][m];
+            return dp[m][n];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
