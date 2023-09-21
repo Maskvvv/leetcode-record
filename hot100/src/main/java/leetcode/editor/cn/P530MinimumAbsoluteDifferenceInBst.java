ed593e7dbe42a034a5cfaf12f892169a4cb4381c
mask <1429855087@qq.com>
2023-09-21 21:50:28 +0800

    530. 二叉搜索树的最小绝对差

---
+++ hot100/src/main/java/leetcode/editor/cn/P530MinimumAbsoluteDifferenceInBst.java
@@ -0,0 +1,89 @@
+package leetcode.editor.cn;
+//给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
+//
+// 差值是一个正数，其数值等于两值之差的绝对值。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [4,2,6,1,3]
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,0,48,null,null,12,49]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数目范围是 [2, 10⁴]
+// 0 <= Node.val <= 10⁵
+//
+//
+//
+//
+// 注意：本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-
+//nodes/ 相同
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉搜索树 二叉树 👍 504 👎 0
+
+import leetcode.editor.cn.model.TreeNode;
+import leetcode.editor.cn.utils.TreeUtils;
+
+//Java：530. 二叉搜索树的最小绝对差
+public class P530MinimumAbsoluteDifferenceInBst {
+    public static void main(String[] args) {
+        Solution solution = new P530MinimumAbsoluteDifferenceInBst().new Solution();
+        // TO TEST
+
+        TreeNode node = TreeUtils.stringToTreeNode("[4,2,6,1,3]");
+        System.out.println(solution.getMinimumDifference(node));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        TreeNode pre;
+        int res = Integer.MAX_VALUE;
+        public int getMinimumDifference(TreeNode root) {
+            dfs(root);
+            return res;
+        }
+
+        public void dfs(TreeNode root) {
+            if (root == null) return;
+
+            dfs(root.left);
+
+            if (pre != null && (root.val - pre.val) < res) res = (root.val - pre.val);
+            pre = root;
+
+            dfs(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
