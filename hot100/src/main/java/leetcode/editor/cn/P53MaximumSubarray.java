863d7742d3feafa4f5fd2902a319fea0d29cd537
mask <1429855087@qq.com>
2023-09-24 17:45:48 +0800

    53. 最大子数组和

--- hot100/src/main/java/leetcode/editor/cn/P53MaximumSubarray.java
+++ hot100/src/main/java/leetcode/editor/cn/P53MaximumSubarray.java
@@ -59,22 +59,17 @@ public class P53MaximumSubarray {
             int n = nums.length;
             if (n == 1) return nums[0];
 
-            int[] dp = new int[n + 1];
-            dp[0] = 0;
-            dp[1] = nums[0];
+            int res = nums[0];
 
-            int max = nums[0];
-            for (int i = 2; i <= n; i++) {
-                if (dp[i - 1] > 0) {
-                    dp[i] = dp[i - 1] + nums[i - 1];
-                } else {
-                    dp[i] = nums[i - 1];
+            for (int i = 1; i < n; i++) {
+                if (nums[i - 1] > 0) {
+                    nums[i] += nums[i - 1];
                 }
-                max = Math.max(max, dp[i]);
+
+                res = Math.max(res, nums[i]);
             }
 
-            return max;
-
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
