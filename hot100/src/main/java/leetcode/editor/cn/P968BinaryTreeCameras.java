c156740889920c696d0c47c2b86eb9edb2b14065
mask <1429855087@qq.com>
2023-09-30 14:27:37 +0800

    968. 监控二叉树

---
+++ hot100/src/main/java/leetcode/editor/cn/P968BinaryTreeCameras.java
@@ -0,0 +1,92 @@
+package leetcode.editor.cn;
+//给定一个二叉树，我们在树的节点上安装摄像头。
+//
+// 节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+//
+// 计算监控树的所有节点所需的最小摄像头数量。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+// 输入：[0,0,null,0,0]
+//输出：1
+//解释：如图所示，一台摄像头足以监控所有节点。
+//
+//
+// 示例 2：
+//
+//
+//
+// 输入：[0,0,null,0,null,0,null,null,0]
+//输出：2
+//解释：需要至少两个摄像头来监视树的所有节点。 上图显示了摄像头放置的有效位置之一。
+//
+//
+// 提示：
+//
+//
+// 给定树的节点数的范围是 [1, 1000]。
+// 每个节点的值都是 0。
+//
+//
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 661 👎 0
+
+import leetcode.editor.cn.model.TreeNode;
+
+//Java：968. 监控二叉树
+public class P968BinaryTreeCameras {
+    public static void main(String[] args) {
+        Solution solution = new P968BinaryTreeCameras().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        int noCover = 0, hasCamera = 1, hasCover = 2;
+        int res = 0;
+
+        public int minCameraCover(TreeNode root) {
+            int rootV = dfs(root);
+            if (rootV == noCover) res++;
+
+            return res;
+        }
+
+        public int dfs(TreeNode root) {
+            if (root == null) return hasCover;
+
+            int left = dfs(root.left);
+            int right = dfs(root.right);
+
+            if (left == noCover || right == noCover) {
+                res++;
+                return hasCamera;
+            } else if (left == hasCamera || right == hasCamera) {
+                return hasCover;
+            } else {
+                return noCover;
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
