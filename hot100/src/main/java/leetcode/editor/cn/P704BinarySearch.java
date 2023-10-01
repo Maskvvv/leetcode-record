82d4bebdaf3376d37e10c62bfe4773399279e310
mask <1429855087@qq.com>
2023-10-01 10:19:18 +0800

    704. 二分查找 (左闭右开)

--- hot100/src/main/java/leetcode/editor/cn/P704BinarySearch.java
+++ hot100/src/main/java/leetcode/editor/cn/P704BinarySearch.java
@@ -38,17 +38,17 @@ public class P704BinarySearch {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int search(int[] nums, int target) {
+            int left = 0, right = nums.length;
 
-            int left = 0, right = nums.length - 1;
-
-            while (left <= right) {
-                int middle = left + (right - left) / 2;
-                if (nums[middle] == target) {
-                    return middle;
-                } else if (nums[middle] > target) {
-                    right = middle - 1;
-                } else if (nums[middle] < target) {
-                    left = middle + 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int num = nums[mid];
+                if (num == target) {
+                    return mid;
+                }else if (num > target) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
                 }
             }
 
