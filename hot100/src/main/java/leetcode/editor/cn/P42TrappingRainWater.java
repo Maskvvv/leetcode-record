36b540e745409e7f2ea11bf1e647ebd1a403278b
mask <1429855087@qq.com>
2023-10-14 13:30:55 +0800

    42. 接雨水(单调栈)

--- hot100/src/main/java/leetcode/editor/cn/P42TrappingRainWater.java
+++ hot100/src/main/java/leetcode/editor/cn/P42TrappingRainWater.java
@@ -32,8 +32,10 @@ package leetcode.editor.cn;
 //
 // Related Topics 栈 数组 双指针 动态规划 单调栈 👍 4736 👎 0
 
-import leetcode.editor.cn.model.TreeNode;
 import leetcode.editor.cn.utils.ArrayUtils;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 //Java：42. 接雨水
 public class P42TrappingRainWater {
@@ -47,28 +49,22 @@ public class P42TrappingRainWater {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int trap(int[] height) {
-            int n = height.length;
-            if (n < 3) return 0;
+            Deque<Integer> stack = new ArrayDeque<>();
 
-            int[] leftMax = new int[n];
-            leftMax[0] = 0;
-            for (int i = 1; i < n; i++) {
-                leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            int res = 0;
+            for (int i = 0; i < height.length; i++) {
+                while (!stack.isEmpty() && height[i] > height[stack.getLast()]) {
+                    Integer mid = stack.removeLast();
+                    if (stack.isEmpty()) break;
+
+                    int left = stack.getLast();
+                    res += (i - left - 1) * (Math.min(height[i], height[left]) - height[mid]);
+                }
+
+                stack.addLast(i);
             }
 
-            int[] rightMax = new int[n];
-            leftMax[n - 1] = 0;
-            for (int i = n - 2; i >= 0; i--) {
-                rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-            }
-
-            int sum = 0;
-            for (int i = 0; i < n; i++) {
-                sum += Math.min(leftMax[i], rightMax[i]);
-            }
-
-            return sum;
-
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
