fccf0e6ba3fda7dac64ba39cd8663d95b4cd881c
mask <1429855087@qq.com>
2023-10-28 11:24:47 +0800

    560. 和为 K 的子数组(前缀和)

---
+++ hot100/src/main/java/leetcode/editor/cn/P560SubarraySumEqualsK.java
@@ -0,0 +1,83 @@
+package leetcode.editor.cn;
+//给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+//
+// 子数组是数组中元素的连续非空序列。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,1,1], k = 2
+//输出：2
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,2,3], k = 3
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 2 * 10⁴
+// -1000 <= nums[i] <= 1000
+// -10⁷ <= k <= 10⁷
+//
+//
+// Related Topics 数组 哈希表 前缀和 👍 2140 👎 0
+
+import leetcode.editor.cn.utils.ArrayUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Java：560. 和为 K 的子数组
+public class P560SubarraySumEqualsK {
+    public static void main(String[] args) {
+        Solution solution = new P560SubarraySumEqualsK().new Solution();
+        // TO TEST
+        System.out.println(solution.subarraySum(ArrayUtils.generateNumArray("[1,1,1]"), 2));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    /*
+        使用前缀和的方法可以解决这个问题，因为我们需要找到和为k的连续子数组的个数。通过计算前缀和，我们可以将问题转化为求解两个前缀和之差等于k的情况。
+        假设数组的前缀和数组为prefixSum，其中prefixSum[i]表示从数组起始位置到第i个位置的元素之和。那么对于任意的两个下标i和j（i < j），
+        如果prefixSum[j] - prefixSum[i] = k，即从第i个位置到第j个位置的元素之和等于k，那么说明从第i+1个位置到第j个位置的连续子数组的和为k。
+
+        通过遍历数组，计算每个位置的前缀和，并使用一个哈希表来存储每个前缀和出现的次数。在遍历的过程中，我们检查是否存在prefixSum[j] - k的前缀和，
+        如果存在，说明从某个位置到当前位置的连续子数组的和为k，我们将对应的次数累加到结果中。
+
+        这样，通过遍历一次数组，我们可以统计出和为k的连续子数组的个数，并且时间复杂度为O(n)，其中n为数组的长度。
+     */
+    public class Solution {
+
+        public int subarraySum(int[] nums, int k) {
+            int count = 0;
+            int sum = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+
+                if (map.get(sum - k) != null) {
+                    count += map.get(sum - k);
+                }
+
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+
+            return count;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
