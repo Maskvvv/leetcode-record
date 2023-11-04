2315cd9d1995aee608177f3a25fa8c0e33c3d1b8
mask <1429855087@qq.com>
2023-11-04 13:17:17 +0800

    25. K 个一组翻转链表(链表)

---
+++ hot100/src/main/java/leetcode/editor/cn/P25ReverseNodesInKGroup.java
@@ -0,0 +1,121 @@
+package leetcode.editor.cn;
+//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+//
+// k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[2,1,4,3,5]
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+//
+//
+//
+//提示：
+//
+//
+// 链表中的节点数目为 n
+// 1 <= k <= n <= 5000
+// 0 <= Node.val <= 1000
+//
+//
+//
+//
+// 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+//
+//
+//
+//
+// Related Topics 递归 链表 👍 2215 👎 0
+
+import leetcode.editor.cn.model.ListNode;
+import leetcode.editor.cn.utils.ListNodeUtils;
+
+//Java：25. K 个一组翻转链表
+public class P25ReverseNodesInKGroup {
+    public static void main(String[] args) {
+        Solution solution = new P25ReverseNodesInKGroup().new Solution();
+        // TO TEST
+
+        ListNode node = solution.reverseKGroup(ListNodeUtils.stringToListNode("[1,2,3,4,5]"), 2);
+        ListNodeUtils.printListNode(node);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            if (k == 1) return head;
+
+            ListNode dummyHead = new ListNode();
+            dummyHead.next = head;
+
+            ListNode pre = dummyHead;
+
+            while (pre != null && pre.next != null) {
+                if (!hasK(pre, k)) {
+                    return dummyHead.next;
+
+                }
+                ListNode left = pre;
+                ListNode current = pre.next;
+                ListNode right = pre.next.next;
+                int i = 0;
+
+                while (i++ < k) {
+                    current.next = left;
+
+                    left = current;
+                    current = right;
+                    if (right == null) break;
+                    right = right.next;
+                }
+
+                //ListNodeUtils.printListNode(dummyHead.next);
+                pre.next.next = current;
+
+                ListNode next = pre.next;
+                pre.next = left;
+
+                pre = next;
+            }
+
+
+            return dummyHead.next;
+        }
+
+        private boolean hasK(ListNode pre, int k) {
+
+            for (int i = 0; i < k; i++) {
+                pre = pre.next;
+                if (pre == null) return false;
+
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
