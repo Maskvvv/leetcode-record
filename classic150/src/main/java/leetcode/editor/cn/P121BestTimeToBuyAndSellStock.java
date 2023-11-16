b438ff20f73cca5092b788f0c75180d53507439b
mask <1429855087@qq.com>
2023-11-16 21:36:37 +0800

    121. 买卖股票的最佳时机(动态规划)

--- classic150/src/main/java/leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
+++ classic150/src/main/java/leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
@@ -49,13 +49,15 @@ public class P121BestTimeToBuyAndSellStock {
     class Solution {
         public int maxProfit(int[] prices) {
             int n = prices.length;
+            int[][] dp = new int[n][2];
 
-            int res = 0;
+            dp[0] = new int[]{0, -prices[0]};
+
             for (int i = 1; i < n; i++) {
-                res = Math.max(res, prices[i] - prices[i - 1]);
-                prices[i]= Math.min(prices[i], prices[i - 1]);
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
             }
-            return res;
+            return dp[n - 1][0];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
