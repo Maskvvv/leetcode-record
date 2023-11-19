e20288a1aeab028ccc9d7be990871a69053b8c00
mask <1429855087@qq.com>
2023-11-19 15:53:32 +0800

    221. 最大正方形(动态规划)

---
+++ classic150/src/main/java/leetcode/editor/cn/P221MaximalSquare.java
@@ -0,0 +1,76 @@
+package leetcode.editor.cn;
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+//,["1","0","0","1","0"]]
+//输出：4
+//
+//
+// 示例 2：
+//
+//
+//输入：matrix = [["0","1"],["1","0"]]
+//输出：1
+//
+//
+// 示例 3：
+//
+//
+//输入：matrix = [["0"]]
+//输出：0
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 300
+// matrix[i][j] 为 '0' 或 '1'
+//
+//
+// Related Topics 数组 动态规划 矩阵 👍 1583 👎 0
+
+//Java：221. 最大正方形
+public class P221MaximalSquare {
+    public static void main(String[] args) {
+        Solution solution = new P221MaximalSquare().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int res = 0;
+
+            int m = matrix.length, n = matrix[0].length;
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == '1') {
+                        if (i == 0 || j == 0) {
+                            dp[i][j] = 1;
+                        } else {
+                            dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+
+                        }
+                        res = Math.max(res, dp[i][j]);
+                    }
+
+                }
+            }
+
+            return res * res;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
