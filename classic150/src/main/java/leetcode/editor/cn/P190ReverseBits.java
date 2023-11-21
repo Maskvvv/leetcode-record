899be208c2a78a98ffeb14a71d28ad23a3dfac94
mask <1429855087@qq.com>
2023-11-21 23:31:14 +0800

    190. 颠倒二进制位(位运算)

--- classic150/src/main/java/leetcode/editor/cn/P190ReverseBits.java
+++ classic150/src/main/java/leetcode/editor/cn/P190ReverseBits.java
@@ -53,15 +53,12 @@ public class P190ReverseBits {
     public class Solution {
         // you need treat n as an unsigned value
         public int reverseBits(int n) {
-            int res = 0;
-
-            for (int i = 0; i < 32 && n != 0; i++) {
-                res |= (n & 1) << (31 - i);
-                n >>>= 1;
-            }
-
-            return res;
-
+            n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+            n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+            n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+            n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+            n = (n >>> 16) | (n << 16);
+            return n;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
