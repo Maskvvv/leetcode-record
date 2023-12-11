5d30afe7f766558f207530133b2c27c839ce2acc
mask <1429855087@qq.com>
2023-12-11 21:09:57 +0800

    104. 二叉树的最大深度(二叉树)

---
+++ classic150/src/main/java/leetcode/editor/cn/P104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,84 @@
+package leetcode.editor.cn;
+//给定一个二叉树 root ，返回其最大深度。
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,null,2]
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数量在 [0, 10⁴] 区间内。
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1754 👎 0
+
+import leetcode.editor.cn.model.TreeNode;
+
+//Java：104. 二叉树的最大深度
+public class P104MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P104MaximumDepthOfBinaryTree().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        int res = 0;
+        public int maxDepth(TreeNode root) {
+            dfs(root, 1);
+
+            return res;
+        }
+
+        public void dfs(TreeNode root, int depth) {
+            if (root == null) return;
+
+            if (root.left == null && root.right == null) {
+                res = Math.max(res, depth);
+            }
+
+            dfs(root.left, depth + 1);
+            dfs(root.right, depth + 1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
