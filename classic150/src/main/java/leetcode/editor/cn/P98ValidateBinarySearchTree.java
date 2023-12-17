e09e9cc2e247abf6017d51889880a2a1924458a5
mask <1429855087@qq.com>
2023-12-17 10:53:43 +0800

    98. 验证二叉搜索树(二叉树)

---
+++ classic150/src/main/java/leetcode/editor/cn/P98ValidateBinarySearchTree.java
@@ -0,0 +1,94 @@
+package leetcode.editor.cn;
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+//
+// 有效 二叉搜索树定义如下：
+//
+//
+// 节点的左子树只包含 小于 当前节点的数。
+// 节点的右子树只包含 大于 当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [2,1,3]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目范围在[1, 10⁴] 内
+// -2³¹ <= Node.val <= 2³¹ - 1
+//
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 2234 👎 0
+
+import leetcode.editor.cn.model.TreeNode;
+import leetcode.editor.cn.utils.TreeUtils;
+
+//Java：98. 验证二叉搜索树
+public class P98ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new P98ValidateBinarySearchTree().new Solution();
+        // TO TEST
+        System.out.println(solution.isValidBST(TreeUtils.stringToTreeNode("[0]")));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        long pre = Long.MIN_VALUE;
+        boolean res = true;
+        public boolean isValidBST(TreeNode root) {
+            inOrder(root);
+
+            return res;
+        }
+
+        public void inOrder(TreeNode root) {
+            if (root == null || !res) {
+                return;
+            }
+
+            inOrder(root.left);
+
+            if ((long) root.val <= pre) {
+                res = false;
+            }
+            pre = root.val;;
+
+            inOrder(root.right);
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
