7cb57ac8951266a77ee0cf21f98beeea11346d4f
mask <1429855087@qq.com>
2024-01-11 21:26:26 +0800

    202. 快乐数(哈希表)

---
+++ classic150/src/main/java/leetcode/editor/cn/P202HappyNumber.java
@@ -0,0 +1,89 @@
+package leetcode.editor.cn;
+//编写一个算法来判断一个数 n 是不是快乐数。
+//
+// 「快乐数」 定义为：
+//
+//
+// 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+// 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+// 如果这个过程 结果为 1，那么这个数就是快乐数。
+//
+//
+// 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 19
+//输出：true
+//解释：
+//1² + 9² = 82
+//8² + 2² = 68
+//6² + 8² = 100
+//1² + 0² + 0² = 1
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 2
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 2³¹ - 1
+//
+//
+// Related Topics 哈希表 数学 双指针 👍 1504 👎 0
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Java：202. 快乐数
+public class P202HappyNumber {
+    public static void main(String[] args) {
+        Solution solution = new P202HappyNumber().new Solution();
+        // TO TEST
+
+        System.out.println(Math.pow(2, 2));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isHappy(int n) {
+
+            Set<Integer> visited = new HashSet<>();
+
+
+            while (n != 1 && !visited.contains(n)) {
+                visited.add(n);
+                n = getNext(n);
+            }
+
+            return n == 1;
+
+        }
+        private int getNext(int n) {
+            int sum = 0;
+
+            while (n > 0) {
+                int a = n % 10;
+                sum += a * a;
+
+                n /= 10;
+            }
+
+            return sum;
+        }
+    }
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
