c211784ebba6177c8bb91f48dbecac9ca468a98e
mask <1429855087@qq.com>
2024-01-24 21:43:51 +0800

    121. 买卖股票的最佳时机(贪心)

--- classic150/src/main/java/leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
+++ classic150/src/main/java/leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
@@ -48,16 +48,15 @@ public class P121BestTimeToBuyAndSellStock {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxProfit(int[] prices) {
-            int n = prices.length;
-            int[][] dp = new int[n][2];
+            int pre = prices[0];
+            int res = 0;
 
-            dp[0] = new int[]{0, -prices[0]};
-
-            for (int i = 1; i < n; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            for (int price : prices) {
+                res = Math.max(price - pre, res);
+                pre = Math.min(pre, price);
             }
-            return dp[n - 1][0];
+
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
