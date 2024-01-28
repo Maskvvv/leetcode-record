4959d7146627dc48ad27c3b607c9406931cb517c
mask <1429855087@qq.com>
2024-01-28 21:15:21 +0800

    12. 整数转罗马数字(哈希)

--- classic150/src/main/java/leetcode/editor/cn/P12IntegerToRoman.java
+++ classic150/src/main/java/leetcode/editor/cn/P12IntegerToRoman.java
@@ -70,9 +70,6 @@ package leetcode.editor.cn;
 //
 // Related Topics 哈希表 数学 字符串 👍 1234 👎 0
 
-import java.util.HashMap;
-import java.util.Map;
-
 //Java：12. 整数转罗马数字
 public class P12IntegerToRoman {
     public static void main(String[] args) {
@@ -84,39 +81,26 @@ public class P12IntegerToRoman {
     class Solution {
 
         public String intToRoman(int num) {
-            int[] key = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-
-            Map<Integer, String> map = new HashMap<>();
-            map.put(1, "I");
-            map.put(4, "IV");
-            map.put(5, "V");
-            map.put(9, "IX");
-            map.put(10, "X");
-            map.put(40, "XL");
-            map.put(50, "L");
-            map.put(90, "XC");
-            map.put(100, "C");
-            map.put(400, "CD");
-            map.put(500, "D");
-            map.put(900, "CM");
-            map.put(1000, "M");
+            int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
 
             StringBuilder sb = new StringBuilder();
             while (num != 0) {
 
-                for (int i = key.length - 1; i >= 0; i--) {
-                    int k = key[i];
-
-                    int count = num / k;
+                for (int i = 0; i < nums.length; i++) {
+                    int count = num / nums[i];
                     if (count == 0) continue;
 
                     for (int j = 0; j < count; j++) {
-                        sb.append(map.get(k));
+                        sb.append(romans[i]);
                     }
-                    num = num % k;
+
+                    num = num % nums[i];
+
                     break;
                 }
+
             }
 
             return sb.toString();
