cbb928f95bc9fbe74aac21c097d5ac5bc0372015
mask <1429855087@qq.com>
2024-01-29 21:10:53 +0800

    58. 最后一个单词的长度(字符串)

---
+++ classic150/src/main/java/leetcode/editor/cn/P58LengthOfLastWord.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;
+//给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+//
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "Hello World"
+//输出：5
+//解释：最后一个单词是“World”，长度为5。
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "   fly me   to   the moon  "
+//输出：4
+//解释：最后一个单词是“moon”，长度为4。
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "luffy is still joyboy"
+//输出：6
+//解释：最后一个单词是长度为6的“joyboy”。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 仅有英文字母和空格 ' ' 组成
+// s 中至少存在一个单词
+//
+//
+// Related Topics 字符串 👍 673 👎 0
+
+import java.util.Arrays;
+
+//Java：58. 最后一个单词的长度
+public class P58LengthOfLastWord {
+    public static void main(String[] args) {
+        Solution solution = new P58LengthOfLastWord().new Solution();
+        // TO TEST
+        String s = "   fly me   to   the moon  ";
+
+        System.out.println(Arrays.toString(s.split("\\s+")));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            String[] split = s.split("\\s+");
+
+
+            return split[split.length - 1].length();
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
