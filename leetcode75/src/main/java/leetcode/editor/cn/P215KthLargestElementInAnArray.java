90614e018e1a0c9e5e56075b3ef2e94167820367
mask <1429855087@qq.com>
2024-02-26 21:34:35 +0800

    215. 数组中的第K个最大元素(堆 / 优先队列)

---
+++ leetcode75/src/main/java/leetcode/editor/cn/P215KthLargestElementInAnArray.java
@@ -0,0 +1,64 @@
+package leetcode.editor.cn;
+//给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+//
+// 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+//
+// 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: [3,2,1,5,6,4], k = 2
+//输出: 5
+//
+//
+// 示例 2:
+//
+//
+//输入: [3,2,3,1,2,4,5,5,6], k = 4
+//输出: 4
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= k <= nums.length <= 10⁵
+// -10⁴ <= nums[i] <= 10⁴
+//
+//
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列） 👍 2412 👎 0
+
+import java.util.PriorityQueue;
+
+//Java：215. 数组中的第K个最大元素
+public class P215KthLargestElementInAnArray {
+    public static void main(String[] args) {
+        Solution solution = new P215KthLargestElementInAnArray().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+            for (int i = 0; i < k; i++) {
+                queue.offer(nums[i]);
+            }
+
+            for (int i = k; i < nums.length; i++) {
+                if (nums[i] > queue.peek()) {
+                    queue.poll();
+                    queue.offer(nums[i]);
+                }
+            }
+
+            return queue.peek();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
