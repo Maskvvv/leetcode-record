ab0396acb7e2b85aaf27411ceef6fe5e2bb345fb
Maskvvv <1429855087@qq.com>
2024-03-11 10:07:44 +0800

    198. 打家劫舍(动态规划 - 一维)

--- classic150/src/main/java/leetcode/editor/cn/P198HouseRobber.java
+++ classic150/src/main/java/leetcode/editor/cn/P198HouseRobber.java
@@ -45,14 +45,13 @@ public class P198HouseRobber {
     class Solution {
         public int rob(int[] nums) {
             int n = nums.length;
-            if (n == 2) {
-                return nums[1];
+            if (n == 1) {
+                return nums[0];
             }
 
             int[] dp = new int[n];
             dp[0] = nums[0];
             dp[1] = Math.max(nums[0], nums[1]);
-
 
             for (int i = 2; i < n; i++) {
                 dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
